6b2ac946d5513f5f80918d81360beeac189bf72d
ankita090497 <ankita090497@gmail.com>
2024-01-17 10:29:35 +0530

    USER-SERVICE:Implemented circuit breaker using resilience4j

--- 05-UserService/UserService/src/main/java/com/lcwd/user/service/UserService/controller/UserController.java
+++ 05-UserService/UserService/src/main/java/com/lcwd/user/service/UserService/controller/UserController.java
@@ -2,10 +2,13 @@ package com.lcwd.user.service.UserService.controller;
 
 import com.lcwd.user.service.UserService.entity.User;
 import com.lcwd.user.service.UserService.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -17,22 +20,40 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User usr = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(usr);
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<User> getUserById(@PathVariable Long userId){
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    public ResponseEntity<User> getUserById(@PathVariable Long userId) {
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
 
     }
 
+
     @GetMapping
-    public ResponseEntity<List<User>> getAllUser(){
+    public ResponseEntity<List<User>> getAllUser() {
         List<User> userList = userService.getAllUser();
         return ResponseEntity.ok(userList);
-    };
+    }
+
+    ;
+
+    // creating fall back method for circuit-breaker
+    public ResponseEntity<User> ratingHotelFallback(Long userId, Exception e) {
+        logger.info("Fallback is executed because service is down : ", e.getMessage());
+        e.printStackTrace();
+        User user = User.builder()
+                .email("abc@gmail.com")
+                .name("Abc")
+                .about("This user is created dummy because some services are down.")
+                .userId(555L).build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 }
